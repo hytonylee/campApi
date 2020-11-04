9da0e5e2b6b9849a9fdfc142602442a05c624e11
Tony Lee <hy.tony.lee@gmail.com>
2020-11-03 23:05:22 -0800

    initialize route

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ dotenv.config({ path: './config/config.env'});
 // initial app
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('Hello from express');
+})
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
