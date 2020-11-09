d941fa8d0f941d7ad7d01cc4af7a8f7ee67a08ea
Tony Lee <hy.tony.lee@gmail.com>
2020-11-08 17:12:57 -0800

    add mongoose and catch at global level

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
-// Route files
-const bootcamps = require('./routes/bootcamps');
+const connectDB = require('./config/db');
 
 // load env var
 dotenv.config({ path: './config/config.env'});
+
+// Connect to DB
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
 
 // initial app
 const app = express();
@@ -16,10 +20,16 @@ if(process.env.NODE_ENV === 'development'){
   app.use(morgan('dev'))
 }
 
-
 // mount router
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Closer server & exit process
+  server.close(() => process.exit(1));
+})
