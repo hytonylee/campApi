55362afaeb09a2351d9e0c9640a59b14d8b92064
Tony Lee <hy.tony.lee@gmail.com>
2020-11-08 17:40:04 -0800

    add colors in cmd line with colors library

--- config/db.js
+++ config/db.js
@@ -8,7 +8,7 @@ const connectDB = async () => {
     useUnifiedTopology: true
   });
 
-  console.log(`MongoDB connect: ${connect.connection.host}`)
+  console.log(`MongoDB connect: ${connect.connection.host}`.cyan.underline.bold)
 }
 
 module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors'); 
 const connectDB = require('./config/db');
+
 
 // load env var
 dotenv.config({ path: './config/config.env'});
@@ -25,11 +27,11 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
-  console.log(`Error: ${err.message}`);
+  console.log(`Error: ${err.message}`.red);
   // Closer server & exit process
   server.close(() => process.exit(1));
 })
