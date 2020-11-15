f917f8ed53f999874f5009c9c4a41d8eb8a5e656
Tony Lee <hy.tony.lee@gmail.com>
2020-11-14 17:32:59 -0800

    add custom error middleware and util

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,4 +1,5 @@
 const Bootcamp = require('../models/Bootcamp');
+const ErrorResponse = require('../utils/errorResponse')
 
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
@@ -27,9 +28,7 @@ exports.getBootcamp = async (req, res, next) => {
     const bootcamp = await Bootcamp.findById(req.params.id);
 
     if(!bootcamp) {
-      return res.status(400).json({
-        success: false
-      })
+      return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404))
     }
 
     res.status(200).json({
@@ -37,9 +36,10 @@ exports.getBootcamp = async (req, res, next) => {
       data: bootcamp
     })
   } catch (err) {
-    res.status(400).json({
-      success: false
-    })
+    // res.status(400).json({
+    //   success: false
+    // })
+    next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404))
   }
   // res.status(200).json({success: true, msg: 'Show a bootcamp'})
 }
